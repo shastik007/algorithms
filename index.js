4ee73b8b04cba979325f8b86c409e8d1dffa8c4f
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-09-03 17:54:36 +0600

    save

---
+++ index.js
@@ -0,0 +1,23 @@
+var list = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25, 26, 27, 28, 29, 30,
+];
+var binary_search = function (list, target) {
+    var start = 0;
+    var end = list.length - 1;
+    var middle;
+    for (var index = 0; start <= end; index++) {
+        middle = Math.floor((end - start) / 2) + start;
+        if (target < list[middle]) {
+            end = middle - 1;
+        }
+        else if (target > list[middle]) {
+            start = middle + 1;
+        }
+        else if (target === list[middle]) {
+            return middle;
+        }
+    }
+    
+};
+console.log(binary_search(list, 3));
