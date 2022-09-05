495380836d18fe8ad2afbbfe09136c39c4e4d23b
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-09-05 15:58:50 +0600

    save

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-const factorial = (number) => {
-  if (number === 1) return number
-  return number * factorial(number - 1)
-}
+ const array = [1,2,3,4,5,6,7,8,1]
 
 
-// console.log(factorial(5));
-
-
-const nested_obj = {
-    name:'Jetigen',
-    count:120,
-    nest:{
-        name:'Tilek',
-        count:100,
-        nest:{
-            name:'Rasul',
-            count:110,
-            nest:{
-                name:'Kuba',
-                count:1100,
-                nest:false
-            }
-        }
+ const sum = (arr = []) => {
+    let current_index = 0
+    const total = arr[current_index]
+    arr.splice(current_index,1)
+    if (arr.length > 0) {
+        current_index += 1
+        return total + sum(arr)
     }
-}
+   
+    return total
+ }
 
 
-const nested_object_trecker = (object) => {
-    const all_count = object.count
-    if (typeof object.nest === 'object') {
-       console.log('it');
-       return all_count + nested_object_trecker(object.nest)
+//  console.log(sum(array));
+
+
+const count_trecker = (arr = []) => {
+    let current_index = 0
+    arr.splice(current_index,1)
+    if (arr.length > 0) {
+        current_index =+ 1
+        return current_index + count_trecker(arr)
     }
-    return all_count 
+    return current_index
 }
 
 
-console.log(nested_object_trecker(nested_obj));
-
-
+console.log(count_trecker(array));
 
 
