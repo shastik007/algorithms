e23df2c673ed492f3fc88871ac0da9d0ed540401
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-10-09 15:20:14 +0600

    quick_sort_finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  const array = [1,2,3,4,5,6,7,8,1]
-
+ const array_2 = [1,3,2,4,5,3,6,4,7,4,8,5,9,7,12,11,14,12]
 
  const sum = (arr = []) => {
     let current_index = 0
@@ -14,7 +14,7 @@
  }
 
 
-//  console.log(sum(array));
+//  console.log(sum(array)); 
 
 
 const count_trecker = (arr = []) => {
@@ -28,6 +28,54 @@ const count_trecker = (arr = []) => {
 }
 
 
-console.log(count_trecker(array));
+// console.log(count_trecker(array));
+
+
+
+
+const get_less = (arr = [],target) => {
+    let less = []
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] < target) {
+            less.push(arr[index])
+        }
+    }
+
+    return less
+}
+
+const get_greater = (arr = [],target) => {
+    let greater = []
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] > target) {
+            greater.push(arr[index])
+        }
+    }
+    return greater
+}
+
+
+const quick_sort = (arr = []) => {
+  if (arr.length < 2) {
+      return arr
+  }else{
+      const pivot = arr[0]
+      const less = get_less(arr,pivot)
+      const greater =  get_greater(arr,pivot)
+      console.log(less,'less');
+      console.log(greater,'greater');
+      return [...quick_sort(less),pivot,...quick_sort(greater)]
+  }
+}
+
+
+console.log(quick_sort(array_2));
+
+
+
+
+//quick sort work with time Obig(n * logn)
+
+
 
 
